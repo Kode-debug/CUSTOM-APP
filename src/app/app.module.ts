fea7fe9d5660a49a55f7c7c3c68c467a2609f652
Nisha Chaube <nisha@Nishas-MacBook-Pro.local>
2019-06-30 12:35:23 -0400

    custom element code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { AppComponent } from './app.component';
+import { CustomButtonComponent } from './custom-button/custom-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomButtonComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [CustomButtonComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector) {
+    const customButton = createCustomElement(CustomButtonComponent, { injector });
+    customElements.define('custom-button', customButton);
+  }
+
+}
